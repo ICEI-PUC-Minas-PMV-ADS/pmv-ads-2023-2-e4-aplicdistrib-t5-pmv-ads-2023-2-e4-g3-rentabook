23a7a16ac98e01143864828098907843d2754f3a
Vitoragp <np.shinigami@gmail.com>
2023-09-20 20:43:59 -0300

    Model Address

---
+++ src/backend/src/main/kotlin/br/puc/projeto/rentabook/models/Address.kt
@@ -0,0 +1,15 @@
+package br.puc.projeto.rentabook.models
+
+import org.springframework.data.annotation.Id
+
+data class Address(
+    @Id
+    val id: String,
+    val cep: String,
+    val street: String,
+    val number: String,
+    val complement: String,
+    val neighborhood: String,
+    val city: String,
+    val state: String,
+)
