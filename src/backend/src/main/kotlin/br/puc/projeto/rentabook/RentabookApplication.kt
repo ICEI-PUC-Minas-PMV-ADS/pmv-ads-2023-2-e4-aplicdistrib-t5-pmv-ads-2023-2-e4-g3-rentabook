fad2eda67ef117ddf5c9a2fc99f30619d30822ec
Vitoragp <np.shinigami@gmail.com>
2023-09-20 20:05:48 -0300

    Commit inicial

---
+++ src/backend/src/main/kotlin/br/puc/projeto/rentabook/RentabookApplication.kt
@@ -0,0 +1,11 @@
+package br.puc.projeto.rentabook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentabookApplication
+
+fun main(args: Array<String>) {
+	runApplication<RentabookApplication>(*args)
+}
---
+++ src/backend/src/test/kotlin/br/puc/projeto/rentabook/RentabookApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.puc.projeto.rentabook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RentabookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
