8743781b4da9fb48c18a9f28eb930f4b388c8fec
Eduardo Lobo <103695641+EdduLobo@users.noreply.github.com>
2023-10-12 19:39:18 -0300

    sss

---
+++ src/frontend-web/src/common/pages/Template.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import NavBar from "../components/Navbar";
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E1DCC5",
+  },
+});
+
+export default function Home() {
+  return (
+    <View style={style.container}>
+      <NavBar />
+    </View>
+  );
+}
