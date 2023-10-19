946b152bbbf1974d4659dac85d8d6489d6ac8d87
Vitoragp <np.shinigami@gmail.com>
2023-10-19 08:43:36 -0300

    Ajustes na estrutura do app

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,10 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
+import * as React from 'react';
 import { AuthProvider } from "./src/contexts/Auth/AuthProvider";
 import Router from "./src/routes";
+
 export default function App() {
   return (
+    <React.StrictMode>
       <AuthProvider>
         <Router />
       </AuthProvider>
+    </React.StrictMode>
   );
 }
