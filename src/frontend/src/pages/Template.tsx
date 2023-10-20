4a7ba14d7e58b38df8ebfddffbb102710f7b2c9d
Vitoragp <np.shinigami@gmail.com>
2023-10-20 13:14:28 -0300

    Revert "Exemplo dropdown"
    This reverts commit ac81132bc0dbf60dde244fdafeb460131ba3565a.

--- src/frontend/src/pages/Template.tsx
+++ src/frontend/src/pages/Template.tsx
@@ -14,15 +14,8 @@ export default function Home() {
   return (
     <View style={style.container}>
       <NavBar />
-
-      <Text>Dropdown #1</Text>
       <Dropdown items={[1, 2, 3]} onSelect={(val) => { console.log(val) }}>
         {(val) => <Text style={{ backgroundColor: "white" }}>{val}</Text>}
-      </Dropdown>
-
-      <Text>Dropdown #2</Text>
-      <Dropdown items={[{ id: 1, name: "pedro" }, { id: 2, name: "carlos" }, { id: 3, name: "maria" }]} onSelect={(val) => { console.log(val.name) }}>
-        {(val) => <Text style={{ backgroundColor: "white" }}>{val.name}</Text>}
       </Dropdown>
     </View>
   );
