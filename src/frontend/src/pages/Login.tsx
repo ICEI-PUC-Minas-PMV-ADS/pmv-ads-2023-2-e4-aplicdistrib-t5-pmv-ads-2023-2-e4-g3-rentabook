ed24e904739f2f2d230773988be0af1232b78a0d
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2023-10-23 17:32:35 -0300

    Implementação da responsividade nas telas de Login e Cadastro

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,43 +1,48 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { AuthContext } from '../contexts/Auth/AuthContext';
 import { StackTypes } from '../routes/StackTypes';
 import Input from '../common/components/Input';
 import PrimaryButton from '../common/components/PrimaryButton';
 import ResponsiveNavbar from "../common/components/ResponsiveNavbar";
 
-
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: "#E1DCC5",
-  },
-  input: {
-    margin: 10,
-  },
-  leftSection: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-  },
-  rightSection: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  welcomeText: {
-    fontSize: 24,
-    textAlign: 'center',
-  }
-});
-
 export default function Login() {
   const auth = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [orientation, setOrientation] = useState(Dimensions.get('window').width > Dimensions.get('window').height ? 'LANDSCAPE' : 'PORTRAIT');
   const navigation = useNavigation<StackTypes>()
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", ({ window: { width, height } }) => {
+      setOrientation(width > height ? 'LANDSCAPE' : 'PORTRAIT');
+    });
+  }, []);
+
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: orientation === 'LANDSCAPE' ? 'row' : 'column-reverse',
+      backgroundColor: "#E1DCC5",
+    },
+    input: {
+      margin: 10,
+    },
+    inputSection: {
+      flex: orientation === 'LANDSCAPE' ? 1 : 0.7,
+      padding: 20,
+      justifyContent: 'center',
+    },
+    welcomeSection: {
+      flex: orientation === 'LANDSCAPE' ? 1 : 0.3,
+      justifyContent: 'center',
+    },
+    welcomeText: {
+      fontSize: 24,
+      textAlign: 'center',
+    }
+  });
 
   const handleLogin = async () => {
     if (email && password) {
@@ -52,7 +57,7 @@ export default function Login() {
   return (
     <ResponsiveNavbar>
       <View style={style.container}>
-        <View style={style.leftSection}>
+        <View style={style.inputSection}>
           <Input
             style={style.input}
             value={email}
@@ -73,7 +78,7 @@ export default function Login() {
             label='Entrar'
           />
         </View>
-        <View style={style.rightSection}>
+        <View style={style.welcomeSection}>
           <Text style={style.welcomeText}>Bem-vindo ao Rentabook!</Text>
         </View>
       </View>
