56d20314bef2b3e5c5f258d77f169025f8282be0
Eduardo Lobo <103695641+EdduLobo@users.noreply.github.com>
2023-10-25 17:36:00 -0300

    u

--- src/frontend/src/pages/Chat.tsx
+++ src/frontend/src/pages/Chat.tsx
@@ -3,7 +3,7 @@ import { View, StyleSheet } from "react-native";
 import ResponsiveNavbar from "../common/components/ResponsiveNavbar";
 import ConversationsList from "../common/components/ConversationList";
 import ChatComponent from "../common/components/ChatComponent";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getToken } from "../other/Storage";
 
 export default function Chat() {
   const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
@@ -14,13 +14,11 @@ export default function Chat() {
   };
 
   useEffect(() => {
-    // Função assíncrona para buscar o token no AsyncStorage
     async function fetchTokenAndUserData() {
       try {
-        const token = await AsyncStorage.getItem(STORAGE_KEY);
+        const token = await getToken();
 
         if (token) {
-          // Realize a solicitação para obter os dados do usuário da API.
           const response = await fetch(
             "https://rentabookapi.azurewebsites.net/user",
             {
@@ -33,7 +31,6 @@ export default function Chat() {
 
           if (response.ok) {
             const data = await response.json();
-            // Extraia o nome do usuário dos dados da resposta e defina-o no estado.
             const userName = data.name;
             setCurrentUser(userName);
           }
@@ -44,7 +41,7 @@ export default function Chat() {
     }
 
     fetchTokenAndUserData();
-  }, []); // Certifique-se de passar uma matriz vazia como segundo argumento para useEffect para que a solicitação seja feita apenas uma vez.
+  }, []);
 
   return (
     <ResponsiveNavbar>
@@ -65,5 +62,3 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
 });
-
-const STORAGE_KEY = "authToken";
